8d8df6dab6c462fd38ae0334f83b32cec28f92b5
Zewyn1 <100469921+Zewyn1@users.noreply.github.com>
2022-03-28 19:00:50 +0300

    Create Zad6a.c

---
+++ Zad6a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int number;
+    float temp, sqrt;
+    printf("Provide the number: \n");
+    scanf("%d", &number);
+    sqrt = number / 2;
+    temp = 0;
+    while (sqrt != temp) {
+        temp = sqrt;
+        sqrt = (number / temp + temp) / 2;
+    }
+
+    printf("The square root of '%d' is '%f'", number, sqrt);
+}
